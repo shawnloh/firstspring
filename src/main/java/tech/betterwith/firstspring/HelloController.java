cdf039a5cb79ace1a2e8e973479cf8292f052e5b
Shawn Loh <tiensoonshawnloh@gmail.com>
2024-10-30 19:30:36 +0800

    Added hellocontroller and hello response

--- src/main/java/tech/betterwith/firstspring/HelloController.java
+++ src/main/java/tech/betterwith/firstspring/HelloController.java
@@ -1,12 +1,21 @@
 package tech.betterwith.firstspring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController()
+@RestController
 public class HelloController {
+    
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name) {
+        return new HelloResponse("Hello, " + name + "!");
+    }
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    public HelloResponse hello() {
+        return new HelloResponse("Hello, World!");
+    }
+    
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name) {
+        return new HelloResponse("Hello " + name + "!");
     }
 }
